1cda23f5bbcddd57e320511cc6656c014072f198
arckoor <33837362+arckoor@users.noreply.github.com>
2022-03-12 14:09:00 +0100

    feature(reply): added function to process rule responses

--- LeeAssets/js/script.js
+++ LeeAssets/js/script.js
@@ -1,4 +1,5 @@
 
+const LEE_DEBUG_MODE = true;
 const LEE_chatlog_container = document.getElementById("lee__chatlog__container");
 const LEE_input = document.getElementById("lee_input");
 const LEE_classes = {
@@ -9,38 +10,161 @@ const LEE_classes = {
 	msg_identifier: "lee__message__identifier",
 	no_select: "lee__no__select",
 	msg_container: "lee__message__container",
+	error: "lee__error__message",
 	selection: "lee__selection"
 };
 const LEE_config_file_name = "./LeeAssets/config/LeeConfig.json";
 const LEE_config = {
 	leeName: null,
-	userName: null
+	userName: null,
+	debugName: null,
+	initMsg: null,
+	initMsgDelay: null,
+	attributes: null
 };
 let LEE_data;
+let LEE_responses;
 
-LEE_load_config();
+LEE();
+
+async function LEE() {
+	LEE_obtain_lock();
+	await LEE_load_config();
+	LEE_sanitize_data();
+	LEE_set_equal_name_length();
+	if (LEE_config.initMsg !== undefined && LEE_config.initMsg != null) {
+		await LEE_construct_message(LEE_config.leeName, LEE_config.initMsg, LEE_config.initMsgDelay);
+	}
+	LEE_discard_lock();
+}
 
 async function LEE_load_config() {
 	LEE_data = await ( await fetch(LEE_config_file_name) ).json();
 	for (const key in LEE_config) {
 		LEE_config[key]= LEE_data.config[key];
 	}
-	LEE_set_equal_name_length();
-	console.log(LEE_config);
+	LEE_responses = LEE_data.rules;
+}
+
+function LEE_pretty_JSON(msg) {
+	return JSON.stringify(msg, null, 4);
 }
 
 function LEE_set_equal_name_length() {
-	// sets both names to an equal length to achieve a consistent message spacing
-	const LEE_max_name_len = Math.max(LEE_config.leeName.length, LEE_config.userName.length);
-	let LEE_length_diff = Math.abs(LEE_config.leeName.length - LEE_config.userName.length);
-	for (let i=0; i<LEE_length_diff; i++) {
-		if (LEE_config.leeName.length === LEE_max_name_len) {
-			LEE_config.userName += String.fromCharCode(160); // &nbsp;
-		} else {
-			LEE_config.leeName += String.fromCharCode(160);
+	let LEE_max_name_len = 0;
+	for (const key in LEE_config) { // compute max length of all names
+		if (key.indexOf("Name") !== -1) {
+			if (LEE_config[key] !== undefined) {
+				if (LEE_config[key].length > LEE_max_name_len) {
+					if (key.indexOf("debug") !== -1) {
+						if (LEE_DEBUG_MODE) { // only include debug if DEBUG_MODE is true, otherwise names could be unnecessarily long
+							LEE_max_name_len = LEE_config[key].length;
+						}
+					} else {
+						LEE_max_name_len = LEE_config[key].length;
+					}
+				}
+			}
+		}
+	}
+	for (const key in LEE_config) { // set all to equal max length
+		if (key.indexOf("Name") !== -1) {
+			if (LEE_config[key] !== undefined) {
+				const LEE_length_diff = Math.abs(LEE_max_name_len - LEE_config[key].length); // get difference between max and name length
+				for (let i=0; i<LEE_length_diff; i++) {
+					LEE_config[key] += String.fromCharCode(160); // &nbsp;
+				}
+			}
 		}
 	}
 }
+
+function LEE_sanitize_data() {
+	for (const topic in LEE_responses) {
+		for (const rule in LEE_responses[topic]) {
+			for (const key of LEE_config.attributes) {
+				if (key in LEE_responses[topic][rule]) {
+					// do something
+				} else {
+					LEE_responses[topic][rule][key] = key === "counter" ? 0 : null;
+				}
+			}
+			if (typeof(LEE_responses[topic][rule]["match"]) === "string") {
+				LEE_responses[topic][rule]["match"] = [LEE_responses[topic][rule]["match"]];
+			}
+			if (typeof(LEE_responses[topic][rule]["response"]) === "string") {
+				LEE_responses[topic][rule]["response"] = [LEE_responses[topic][rule]["response"]];
+			}
+			if (LEE_DEBUG_MODE) {
+				LEE_construct_message(LEE_config.debugName, rule + " " + LEE_pretty_JSON(LEE_responses[topic][rule]));
+			}
+		}
+	}
+	console.log(LEE_responses);
+}
+
+function LEE_index_from_string(data, responseKey) {
+	const LEE_subkeys = responseKey.split(".");
+	let LEE_current_object = data;
+	for (const key of LEE_subkeys) {
+		LEE_current_object = LEE_current_object[key];
+	}
+	return LEE_current_object;
+}
+
+async function LEE_print_debug_error(msg) {
+	const LEE_debug_msg = await LEE_construct_message(LEE_config.debugName, msg);
+	LEE_debug_msg.classList.add(LEE_classes.error);
+}
+
+async function LEE_reply_from_key(key, previousKey=null) {
+	const LEE_cr = LEE_index_from_string(LEE_responses, key); // cr -> current_response
+	let LEE_reply = null;
+	let LEE_skip = false;
+	if (LEE_cr !== undefined) {
+		if (LEE_cr["response"] === null && LEE_cr["ref"] !== null) {
+			LEE_skip = true;
+		}
+		if (LEE_cr["response"] !== null || LEE_skip) {
+			if (!LEE_skip && LEE_cr["random"]) { // pick a random reponse
+				const LEE_random_index = Math.floor(Math.random() * (LEE_cr["response"].length));
+				LEE_reply = LEE_cr["response"][LEE_random_index];
+			} else {
+				if (!LEE_skip && LEE_cr["counter"] !== LEE_cr["response"].length) { // check if the counter is at the arrays last index
+					LEE_reply = LEE_cr["response"][LEE_cr["counter"]];
+					LEE_cr["counter"]++;
+				} else {
+					if (LEE_cr["ref"] !== null) { // at the end of the array, if there is a ref redirect there
+						let LEE_new_key = LEE_cr["ref"];
+						if (LEE_new_key.indexOf("this") !== -1) { // if in same subkey, take original key and append everything after this.
+							LEE_new_key = key.substring(0, key.lastIndexOf(".")+1) + LEE_new_key.replace("this.", "");
+						}
+						if (previousKey === key) { // will recurse until infinity (or until the stack runs out of space)
+							if (LEE_DEBUG_MODE) {
+								LEE_print_debug_error(`Recursion detected:\n${key} ${LEE_pretty_JSON(LEE_index_from_string(LEE_responses, key))}`);
+							}
+							return;
+						}
+						if (previousKey === null) {
+							previousKey = key; // set up recursion safety
+						}
+						return LEE_reply_from_key(LEE_new_key, previousKey); // recurse to resolve new key
+					} else {
+						LEE_reply = LEE_cr["response"][LEE_cr["counter"]-1]; // repeat the last entry the array has
+					}
+				}
+			}
+		}
+	}
+	if (LEE_reply !== null) {
+		return LEE_reply;
+	}
+	if (LEE_DEBUG_MODE) {
+		LEE_print_debug_error(`No response defined for:\n${key} : ${LEE_pretty_JSON(LEE_index_from_string(LEE_responses, key))}`);
+	}
+	return;
+}
+
 
 async function LEE_construct_message(from, message, delay=0) {
 	const LEE_from_user = from === LEE_config.userName;
@@ -61,20 +185,28 @@ async function LEE_construct_message(from, message, delay=0) {
 	await LEE_sleep(delay);
 	LEE_chatlog_container.appendChild(LEE_chatlog_msg_container);
 	LEE_scroll_down();
+	return LEE_chatlog_msg_container;
+}
+
+function LEE_sanitize_input(message) {
+	// https://stackoverflow.com/a/3286919
+	message = message.trim(); // remove trailing and preceeding spaces
+	message = message.replace(/\s\s+/g, " ").toLowerCase(); // remove multiple spaces
+	return message;
 }
 
 async function LEE_get_input() {
 	LEE_obtain_lock();
 	let LEE_user_input = LEE_input.value;
-	if (LEE_user_input === "") {
+	LEE_input.value = null;
+	let LEE_treated_input = LEE_sanitize_input(LEE_user_input);
+	if (LEE_treated_input === "" || LEE_treated_input === " ") {
 		LEE_discard_lock();
 		return;
 	}
-	// TODO also validate if a string is just "   " empty spaces shush
-	LEE_input.value = null;
 	await LEE_construct_message(LEE_config.userName, LEE_user_input);
 	// do something with the input here (reply with something)
-	await LEE_construct_message(LEE_config.leeName, "LEE replies with: " + LEE_user_input, 200+Math.floor(Math.random()*201)-100);
+	await LEE_construct_message(LEE_config.leeName, "LEE replies with: " + LEE_treated_input, 200+Math.floor(Math.random()*201)-100);
 	LEE_discard_lock();
 	LEE_scroll_down();
 }
